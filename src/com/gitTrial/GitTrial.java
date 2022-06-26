6a38e341d59a6910c43fbf0a8ee5e9d8992d50c3
BaskaranEswaran <92baskar@gmail.com>
2022-06-26 18:07:53 +0530

    First Bush

---
+++ src/com/gitTrial/GitTrial.java
@@ -0,0 +1,21 @@
+package com.gitTrial;
+
+public class GitTrial {
+	private void method1() {
+
+	}
+	
+	private void method2() {
+
+	}
+	
+	private void method3() {
+
+	}
+
+	private void method4() {
+
+	}
+
+
+}
